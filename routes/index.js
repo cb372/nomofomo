95241e6459f668f9961d6832e945e11898f81d03
Chris Birchall <chris.birchall@guardian.co.uk>
2015-02-26 16:31:06 +0000

    Made a start on backend
    Grabs data from Ophan and CAPI and munges it together

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var q = require('q');
 var router = express.Router();
 var ophan = require('../lib/ophan.js');
+var capi = require('../lib/capi.js');
+var timeline = require('../lib/timeline.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  ophan.getMostReadArticles(function(error, data) {
-    res.render('index', { title: 'Express', articles: data });
-  });
+  timeline.getTimeline()
+  .then(function(articles) {
+    res.json(articles);
+  })
+  .catch(function(err) {
+    res.render('error', { 
+      message: err.message,
+      error: err 
+    });
+  })
 });
 
 module.exports = router;
