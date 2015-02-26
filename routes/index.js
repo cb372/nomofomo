3b36b67826d8c809e9b9eb29847de36ccc85a464
Chris Birchall <chris.birchall@guardian.co.uk>
2015-02-26 14:57:14 +0000

    Grab some data from Ophan, send it to browser

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var ophan = require('../lib/ophan.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  ophan.getMostReadArticles(function(error, data) {
+    res.render('index', { title: 'Express', articles: data });
+  });
 });
 
 module.exports = router;
